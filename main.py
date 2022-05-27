793b0850b4deb66e824b5bef6e80cdf1e2e38b93
SoulHunteer <seregalutovinov@yandex.ru>
2022-05-27 11:57:42 +0300

    Version 1.1 Ñadd check error

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from PyQt5.QtCore import *
 
 
 class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
+    # В КОДЕ НИЖЕ УКАЗЫВАЮТСЯ ЗНАЧЕНИЯ ПО УМОЛЧАНИЮ, КОТОРЫЕ ПРИ ЖЕЛАНИИ МОЖНО ИЗМЕНИТЬ, КОММЕНТЫ Я ОСТАВИЛ
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.action)
         self.lineEdit.setText(
-            r'C:\Users\Sergey\PycharmProjects\NIITP\Электронный_журнал_статистики_ЕТРИС_ДЗЗ_2022.xlsx')
-        self.lineEdit_2.setText(r'НКПОР-Р-В (Восточный)')
-        self.dateEdit.setDate(QDate(QDate.year(QDate.currentDate()), 1, 1))
-        self.dateEdit_2.setDate(QDate.currentDate())
-
+            r'C:\Users\Sergey\PycharmProjects\NIITP\Электронный_журнал_статистики_ЕТРИС_ДЗЗ_2022.xlsx')  # Здесь выбирается дефолтный файл Excel
+        self.lineEdit_2.setText(r'НКПОР-Р-В (Восточный)')  # Здесь выбирается дефолтный лист Excel
+        self.dateEdit.setDate(
+            QDate(QDate.year(QDate.currentDate()), 1, 1))  # Тут выставляется дата, в одну строчку не опишешь как :)
+        self.dateEdit_2.setDate(QDate.currentDate())  # Тут выставляется дата, в одну строчку не опишешь как :)
+        self.spinBox.setValue(7)  # Тут выставляется значение по умолчанию для колонки с ошибками
+        self.spinBox_2.setValue(12)  # Тут выставляется значение по умолчанию для колонки с комментариями
 
     def action(self, path):
         self.book = self.lineEdit_2.text()
@@ -35,7 +38,8 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
             for i, row in enumerate(sheet.rows):
                 if i == 0:
                     continue
-                if row[0].value is not None and row[int(self.column_comm)].value is not None and row[7].value != 0 and row[0].value.date() >= self.from_date and row[0].value.date() <= self.to_date:
+                if row[0].value is not None and row[int(self.column_comm)].value is not None and row[7].value != 0 and \
+                        row[0].value.date() >= self.from_date and row[0].value.date() <= self.to_date:
                     result_list.append([row[0].value, row[12].value])
 
             workbook = Workbook()
@@ -49,15 +53,16 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
             new_book.save(current_time + '_' + 'отчёт_об_ошибках.xlsx')
             QMessageBox.about(self, "Успешно", f"Файл {name} сформирован успешно!")
         except Exception as e:
+            QMessageBox.about(self, "Ошибка!", e.__str__())
             print(e)
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
+    window = ExampleApp()
+    window.show()
+    app.exec_()
 
 
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
+if __name__ == '__main__':
+    main()
